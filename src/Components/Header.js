93b5488134ccfa9c6760cce748ab1db7291849ca
Arum Galadima <arum.galadima@jacobseye.com>
2019-03-14 18:43:00 -0400

    commit commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
 import AngLogo from '../Images/white-ang-logo.png';
+import Heading from '../Images/LandingPage-HeaderImage 2600x810.png';
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <div className="smallBG">
+        <div className="centerAll">
           <div className="BelowHeader">
-            <img className="resizeImageBelow" src={'https://now.goang.com/wp-content/uploads/2018/11/F35_ThankYou.jpg'} alt="logo" />
-          </div>
-          <div className="centered">
-            <img className="resizeImage" src={AngLogo} alt="logo" />
-          </div>
-          <div className="top-left">
-            Congratulations on taking the first step! Discover the benefits of joining the Air National Guard:
-            Educational Benefits . World-Class Training . Global and Local Missions <br/><br/>
-            Click the button to speak with an Air National Guard representative now!
-          </div>
-          <div className="top-right">
-            <a className="callAng" href="tel:6168819326">800-TO-GO-ANG</a>
+            <a href='tel:8008646264'><img className="resizeImageBelow" src={Heading} alt="logo" /></a>
           </div>
         </div>
-        <div className="takeoff">READY FOR TAKEOFF?</div>
+        <div className="tagline">Get world-class training, exposure to the most advanced technology on the planet, and missions that matter to communities.<br/> With a part-time commitment, you’ll have the flexibility to live life on your terms - meaning more time with family, pursuing an education, maintaining a civilian career, or all of the above. With the Air National Guard, all the choices are yours.</div>
+        <div className="takeoff"><a className="telephone" href="tel:8008646264">CLICK TO CALL 800-TO-GO-ANG</a></div>
+        <br/>
+        <div className="tagline">No time to talk? No problem.<br/>Fill out the form below and and we’ll reach out to you.</div>
       </div>
     );
   }
